31333c324e85bf8cacfe9fe07ef119cb44e62f45
PI-KT <kyoungtaek.oh@fnsusa.com>
2023-12-13 13:11:25 -0800

    add script

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
-import React from "react";
 import "./App.css";
+import React, { useState, useEffect } from "react";
+import { Providers, ProviderState } from "@microsoft/mgt-element";
+import { Agenda, Login } from "@microsoft/mgt-react";
+
+function useIsSignedIn(): [boolean] {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
+  useEffect(() => {
+    const updateState = () => {
+      const provider = Providers.globalProvider;
+      setIsSignedIn(provider && provider.state === ProviderState.SignedIn);
+    };
+
+    Providers.onProviderUpdated(updateState);
+    updateState();
+
+    return () => {
+      Providers.removeProviderUpdatedListener(updateState);
+    };
+  }, []);
+
+  return [isSignedIn];
+}
 
 function App() {
+  const [isSignedIn] = useIsSignedIn();
+
   return (
     <div className="App">
-      <h1>Hi</h1>
-      <p>Test Branch</p>
+      <header>
+        <Login />
+      </header>
+      <div className="row">
+        <div className="column">{isSignedIn && <Agenda />}</div>
+      </div>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Providers } from "@microsoft/mgt-element";
+import { Msal2Provider } from "@microsoft/mgt-msal2-provider";
+
+Providers.globalProvider = new Msal2Provider({
+  clientId: "63404d54-f1ed-493e-a065-53d7c8a108a7",
+  scopes: [
+    "calendars.read",
+    "user.read",
+    "openid",
+    "profile",
+    "people.read",
+    "user.readbasic.all",
+  ],
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
